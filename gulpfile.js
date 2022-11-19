25cf11b9e40fde9305f1129f8e42d4db362788a2
Przemysław Fikas <fikasp@gmail.com>
2022-11-19 10:38:41 +0100

    rebuild folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 var gulp = require('gulp')
+var concat = require('gulp-concat')
 var sass = require('gulp-sass')(require('sass'))
-// var concat = require('gulp-concat')
 var webserver = require('gulp-webserver');
 
+gulp.task('assets', () => {
+    return gulp.src('src/assets/**')
+    .pipe(gulp.dest('dist/assets'))
+})
 
 gulp.task('template', () => {
     return gulp.src('src/index.html')
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('scripts', () => {
-    return gulp.src('src/js/*.js')
-    // .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('dist'))
-})
-
 gulp.task('styles', () => {
-    return gulp.src('src/scss/styles.scss')
+    return gulp.src('src/styles/styles.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('assets', () => {
-    return gulp.src('src/assets/**')
-    .pipe(gulp.dest('dist/assets'))
+gulp.task('scripts', () => {
+    return gulp.src([
+        'src/scripts/jquery.js',
+        'src/scripts/scripts.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('webserver', () => {
@@ -36,17 +37,17 @@ gulp.task('webserver', () => {
 
 gulp.task('watch', () => {
     gulp.watch('src/index.html', gulp.series('template'))
-    gulp.watch('src/js/scripts.js', gulp.series('scripts'))
-    gulp.watch('src/scss/**/*.scss', gulp.series('styles'))
+    gulp.watch('src/scripts/scripts.js', gulp.series('scripts'))
+    gulp.watch('src/styles/**/*.scss', gulp.series('styles'))
     gulp.watch('src/assets/**', gulp.series('assets'))
 })
 
 gulp.task('default', 
     gulp.parallel(
+        'assets',
         'template', 
         'styles', 
         'scripts',
-        'assets',
         'webserver',
         'watch', 
     )
